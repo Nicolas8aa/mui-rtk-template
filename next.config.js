4775015efb41593d16c22c794c3a23a34096370e
Nicolas C <nicolascuellar2305@gmail.com>
2023-05-07 18:39:49 -0500

    Solving @mui/material imports not working on Nextjs 13

--- next.config.js
+++ next.config.js
@@ -12,17 +12,19 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  modularizeImports: {
-    "@mui/material": {
-      transform: "@mui/material/{{member}}",
-    },
-    "@mui/icons-material": {
-      transform: "@mui/icons-material/{{member}}",
-    },
-    "@mui/styles": {
-      transform: "@mui/styles/{{member}}",
-    },
-  },
+  transpilePackages: ["@mui/material/css"],
+  // ! Not working on Next.js 13
+  // modularizeImports: {
+  //   "@mui/material": {
+  //     transform: "@mui/material/{{member}}",
+  //   },
+  //   "@mui/icons-material": {
+  //     transform: "@mui/icons-material/{{member}}",
+  //   },
+  //   "@mui/styles": {
+  //     transform: "@mui/styles/{{member}}",
+  //   },
+  // },
 };
 
 module.exports = nextConfig;
